0ad4287f296d5c6396130a689a6a44db8bf47a6d
Aarav3208 <160886636+Aarav3208@users.noreply.github.com>
2024-06-12 13:19:29 +0530

    done

---
+++ xreactx/src/comps/secondpage.js
@@ -0,0 +1,14 @@
+import './secondpage.css';
+import React from 'react';
+
+function SecondPage (props) {
+    return (
+        <div class="secondpage">
+            <img class="secondpageimage" src={props.image} alt={props.heading}/>
+            <h3 class="secondpageheading">{props.heading}</h3>
+            <p class="secondpagepara">{props.para}</p>
+        </div>
+    )
+}
+
+export default SecondPage;
